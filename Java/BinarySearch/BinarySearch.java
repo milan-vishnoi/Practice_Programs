31b0110bb70bf611e7b4ea7e2e7a0ebab18f5a76
Milan Vishnoi <milan.vish10@gmail.com>
2024-03-06 01:29:34 +0530

    Added new folder for Java and withing that added BinarySearch program.

---
+++ Java/BinarySearch/BinarySearch.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class BinarySearch {
+
+    public static void main(String args[]) {
+        int arr[] = { 2, 45, 57, 78, 109, 113, 115, 117 };
+        int element;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the element to be searched:");
+        element = sc.nextInt();
+        sc.close();
+        boolean found = binarySearch(arr, element);
+
+        if (found)
+            System.out.println("Element found!!");
+        else
+            System.out.println("Element not found in array!!");
+    }
+
+    public static boolean binarySearch(int arr[], int element) {
+        for (int low = 0, high = arr.length - 1; low <= high;) {
+            int middle = (low + high) / 2;
+            if (element == arr[middle])
+                return true;
+            else if (element > arr[middle])
+                low = middle + 1;
+            else
+                high = middle - 1;
+        }
+        return false;
+    }
+
+}
---
+++ Java/BinarySearch/BinarySearchTest.java
@@ -0,0 +1,39 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BinarySearchTest {
+    @Test
+    public void singleArrayFound() {
+        int arr[] = { 4 };
+        int element = 4;
+        Assert.assertTrue(BinarySearch.binarySearch(arr, element));
+    }
+
+    @Test
+    public void singleArrayNotFound() {
+        int arr[] = { 10 };
+        int element = 4;
+        Assert.assertFalse(BinarySearch.binarySearch(arr, element));
+    }
+
+    @Test
+    public void normalArrayNotFound() {
+        int arr[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18 };
+        int element = 5;
+        Assert.assertFalse(BinarySearch.binarySearch(arr, element));
+    }
+
+    @Test
+    public void normalArrayFound() {
+        int arr[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18 };
+        int element = 4;
+        Assert.assertTrue(BinarySearch.binarySearch(arr, element));
+    }
+
+    @Test
+    public void emptyArray() {
+        int arr[] = {};
+        int element = 4;
+        Assert.assertFalse(BinarySearch.binarySearch(arr, element));
+    }
+}

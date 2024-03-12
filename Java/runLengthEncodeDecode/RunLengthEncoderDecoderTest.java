d227ee0999848afaa64d869c00f419e132678faf
Milan Vishnoi <milan.vish10@gmail.com>
2024-03-13 00:32:48 +0530

    Added lowercase alphabets to test case

--- Java/runLengthEncodeDecode/RunLengthEncoderDecoderTest.java
+++ Java/runLengthEncodeDecode/RunLengthEncoderDecoderTest.java
@@ -10,15 +10,15 @@ public class RunLengthEncoderDecoderTest {
     @Test
     public void simpleEncoder()
     {
-        String str = "WWWMMMKKK";
-        Assert.assertEquals("W3M3K3",encoderDecoder.encode(str));
+        String str = "WWWMMMKKKkkk";
+        Assert.assertEquals("W3M3K3k3",encoderDecoder.encode(str));
     }
 
     @Test
     public void encodeStringWithUniqueCharacters()
     {
-        String str = "WMK";
-        Assert.assertEquals("W1M1K1", encoderDecoder.encode(str));
+        String str = "WMmK";
+        Assert.assertEquals("W1M1m1K1", encoderDecoder.encode(str));
     }
 
     @Test
@@ -31,35 +31,35 @@ public class RunLengthEncoderDecoderTest {
     @Test
     public void encodeLongString()
     {
-        String str = "WWWWWWWWWWWWWWWWWWWWWWWWWWMMMMMMMMMMMMMMMMMMMMMMMKKKKKKKKKKKKKKKKKKKKWWW";
-        Assert.assertEquals("W26M23K20W3", encoderDecoder.encode(str));
+        String str = "WWWWWWWWWWWWWWWWWWWWWWWWWWMMMmmmMMMMMMMMMMMMMMMMMMMMKKKKKKKKKKKKKKKKKKKKWWW";
+        Assert.assertEquals("W26M3m3M20K20W3", encoderDecoder.encode(str));
     }
 
     @Test
     public void simpleDecoder()
     {
-        String str = "W3M3K3";
-        Assert.assertEquals("WWWMMMKKK",encoderDecoder.decode(str));
+        String str = "W3w2M3K3";
+        Assert.assertEquals("WWWwwMMMKKK",encoderDecoder.decode(str));
     }
 
     @Test
     public void decodeStringWithUniqueCharacters()
     {
-        String str = "W1M1K1";
-        Assert.assertEquals("WMK", encoderDecoder.decode(str));
+        String str = "W1M1K1u1";
+        Assert.assertEquals("WMKu", encoderDecoder.decode(str));
     }
 
     @Test
     public void decodeMorethan10Characters()
     {
-        String str = "W12M12N3";
-        Assert.assertEquals("WWWWWWWWWWWWMMMMMMMMMMMMNNN", encoderDecoder.decode(str));
+        String str = "W12M12N3m3";
+        Assert.assertEquals("WWWWWWWWWWWWMMMMMMMMMMMMNNNmmm", encoderDecoder.decode(str));
     }
     @Test
     public void decodeMorethan100Characters()
     {
-        String str = "W101M12N3";
-        Assert.assertEquals("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"+
+        String str = "w3W101M12N3";
+        Assert.assertEquals("wwwWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"+
         "MMMMMMMMMMMMNNN", encoderDecoder.decode(str));
     }
 
@@ -67,17 +67,17 @@ public class RunLengthEncoderDecoderTest {
     public void testEncoding()
     {
         char mode = 'E';
-        String str = "WWWWWWWWWWWWWWWWWWWWWWWWWWMMMMMMMMMMMMMMMMMMMMMMMKKKKKKKKKKKKKKKKKKKKWWW";
-        Assert.assertEquals("W26M23K20W3", encoderDecoder.encoderDecoder(mode, str));
+        String str = "WWWWWWWWWWWWWWWWWWWWWWWWWWMMMMMMMMMMMMMMMMMMMMMMMKKKKKKKKKKKKKKKKKKKKWWWccc";
+        Assert.assertEquals("W26M23K20W3c3", encoderDecoder.encoderDecoder(mode, str));
     }
 
     @Test
     public void testDecoding()
     {
         char mode = 'D';
-        String str = "W101M12N3";
+        String str = "W101M12N3d3";
         Assert.assertEquals("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"+
-        "MMMMMMMMMMMMNNN", encoderDecoder.encoderDecoder(mode,str));
+        "MMMMMMMMMMMMNNNddd", encoderDecoder.encoderDecoder(mode,str));
     }
 
 

45b3649c103f859dd306b085b485d4a9cc406ba9
Milan Vishnoi <milan.vish10@gmail.com>
2024-04-20 03:14:15 +0530

    Added code to take the user input and build the PERT Chart accordingly

--- Java/pertChart/graph.java
+++ Java/pertChart/graph.java
@@ -1,83 +1,100 @@
 package pertChart;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class graph {
 
     static int pathNumber = 0;
-    public static void main(String args[])
-    {
-       Node work1 = new Node(1,5,new ArrayList<Node>());
-       Node work2 = new Node(2,10,new ArrayList<Node>(Arrays.asList(work1)));
-       Node work3 = new Node(3,12,new ArrayList<Node>(Arrays.asList(work1)));
-       Node work4 = new Node(4,10,new ArrayList<Node>(Arrays.asList(work1, work3)));
-       Node work5 = new Node(5,10,new ArrayList<Node>(Arrays.asList(work2)));
-       Node work6 = new Node(6,10,new ArrayList<Node>(Arrays.asList(work2,work3,work4)));
-       Node work7 = new Node(7,10,new ArrayList<Node>(Arrays.asList(work4)));
-       Node work8 = new Node(8,10,new ArrayList<Node>(Arrays.asList(work5,work6)));
-       Node work9 = new Node(9,10,new ArrayList<Node>(Arrays.asList(work6,work7,work8)));
 
-       Node[] works = {work1,work2,work3,work4,work5,work6,work7,work8,work9};
-    
-       System.out.println("Table:");
-       for(Node work : works)
-       {
-        System.out.println(work.toString());
-       }
-       System.out.println("\nPaths:");
-       findPath(findStart(works), findEnd(works), findStart(works).getWorkId()+"");
+    public static void main(String args[]) {
+
+        HashMap<Integer, Node> works = new HashMap<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of works:");
+        int noOfNodes = sc.nextInt();
+        Node[] workList = new Node[noOfNodes];
+
+        for (int i = 0; i < noOfNodes; i++) {
+            ArrayList<Node> dependsOnList = new ArrayList<>();
+            System.out.printf("Enter workId & time for work%d's:", i + 1);
+            int workId = sc.nextInt();
+            int time = sc.nextInt();
+            System.out.printf("Enter works on which work%d depends:", i + 1);
+            String dependsOn = sc.next();
+            if (dependsOn.equals("-1")) {
+
+            } else {
+                int[] nodes;
+                if (dependsOn.contains(",")) {
+                    nodes = Arrays.stream(dependsOn.split(",")).mapToInt((n) -> Integer.parseInt(n)).toArray();
+                    for (int j = 0; j < nodes.length; j++)
+                        dependsOnList.add(works.get(nodes[j]));
+                } else {
+                    dependsOnList.add(works.get(Integer.parseInt(dependsOn)));
+                }
+
+            }
+
+            Node work = new Node(workId, time, dependsOnList);
+            works.put(workId, work);
+            workList[i] = work;
+
+        }
+
+        System.out.println("Table:");
+        for (Node work : workList) {
+            System.out.println(work.toString());
+        }
+        System.out.println("\nPaths:");
+        findPath(findStart(workList), findEnd(workList), findStart(workList).getWorkId() + "");
 
     }
 
-    public static Node findStart(Node[] works)
-    {
-        for(Node work : works)
-        {
-            if(work.getDependsOn().isEmpty())
-            return work;
+    public static Node findStart(Node[] works) {
+        for (Node work : works) {
+            if (work.getDependsOn().isEmpty())
+                return work;
         }
 
         return null;
     }
 
-    public static Node findEnd(Node[] works)
-    {
-        for(Node work : works)
-        {
-            if(work.getChildren().isEmpty())
-            return work;
+    public static Node findEnd(Node[] works) {
+        for (Node work : works) {
+            if (work.getChildren().isEmpty())
+                return work;
         }
 
         return null;
     }
 
-    public static void findPath(Node start, Node end, String path)
-    {
+    public static void findPath(Node start, Node end, String path) {
         // System.out.println("Start:"+start.toString());
         // System.out.println("End:"+end.toString());
 
-        if(start.equals(end))
-        {
-            System.out.println("Path"+(++pathNumber)+": "+path);
+        if (start.equals(end)) {
+            System.out.println("Path" + (++pathNumber) + ": " + path);
             return;
         }
-        
+
         start.getChildren().forEach((child) -> {
-            String p = path+"->"+child.getWorkId();
-            findPath(child,end,p);
+            String p = path + "->" + child.getWorkId();
+            findPath(child, end, p);
 
         });
-        //Either of the two for loops will work
+        // Either of the two for loops will work
         // for(Node child : start.getChildren())
         // {
-        //  
-        //      String p = path +"->"+child.getWorkId();
-        //      findPath(child,end,p);
+        //
+        // String p = path +"->"+child.getWorkId();
+        // findPath(child,end,p);
         // }
 
     }
 }
-
 
 class Node {
     private int workId;
@@ -85,96 +102,76 @@ class Node {
     private ArrayList<Node> children;
     private ArrayList<Node> dependsOn;
 
-    public Node(int workId,int time,ArrayList<Node> dependsOn)
-    {
-         this.workId = workId;
-         this.time = time;
-         this.children = new ArrayList<Node>();
-         this.dependsOn = new ArrayList<Node>(dependsOn);
-         this.dependsOn.forEach((parent) -> updateParent(parent)); 
+    public Node(int workId, int time, ArrayList<Node> dependsOn) {
+        this.workId = workId;
+        this.time = time;
+        this.children = new ArrayList<Node>();
+        this.dependsOn = new ArrayList<Node>(dependsOn);
+        this.dependsOn.forEach((parent) -> updateParent(parent));
     }
 
-    public int getWorkId()
-    {
-       return this.workId;
+    public int getWorkId() {
+        return this.workId;
     }
 
-    public void setWorkId(int workId)
-    {
-       this.workId =  workId;
+    public void setWorkId(int workId) {
+        this.workId = workId;
     }
 
-    public int getTime()
-    {
+    public int getTime() {
         return this.time;
     }
 
-    public void setTime(int time)
-    {
+    public void setTime(int time) {
         this.time = time;
     }
 
-
-    public ArrayList<Node> getChildren()
-    {
+    public ArrayList<Node> getChildren() {
         return this.children;
     }
 
-    public void setChild(Node node)
-    {
+    public void setChild(Node node) {
         this.children.add(node);
     }
 
-    public ArrayList<Node> getDependsOn()
-    {
+    public ArrayList<Node> getDependsOn() {
         return this.dependsOn;
     }
 
-    public void setDependsOn(ArrayList<Node> dependsOn)
-    {
+    public void setDependsOn(ArrayList<Node> dependsOn) {
         this.dependsOn = dependsOn;
     }
 
-
-
-    private void updateParent(Node node)
-    {   
+    private void updateParent(Node node) {
         node.setChild(this);
     }
 
-    public boolean equals(Node node)
-    {
-         if(this.workId == node.getWorkId())
-         return true;
-         else
-         return false;
+    public boolean equals(Node node) {
+        if (this.workId == node.getWorkId())
+            return true;
+        else
+            return false;
     }
 
-
-    public String toString()
-    {   
-        String dependStr = "["; 
+    public String toString() {
+        String dependStr = "[";
         String childStr = "[";
-        for(int i= 0;i<this.dependsOn.size() ;i++ )
-        dependStr+=this.dependsOn.get(i).getWorkId()+",";
-        
-        if(dependStr.length()>1)
-        dependStr = dependStr.substring(0,dependStr.length()-1);
+        for (int i = 0; i < this.dependsOn.size(); i++)
+            dependStr += this.dependsOn.get(i).getWorkId() + ",";
 
-        dependStr+="]";
+        if (dependStr.length() > 1)
+            dependStr = dependStr.substring(0, dependStr.length() - 1);
 
-        for(int i= 0;i<this.children.size() ;i++ )
-        childStr+=this.children.get(i).getWorkId()+",";
-        
-        if(childStr.length()>1)
-        childStr = childStr.substring(0,childStr.length()-1);
+        dependStr += "]";
 
-        childStr+="]";
+        for (int i = 0; i < this.children.size(); i++)
+            childStr += this.children.get(i).getWorkId() + ",";
 
-        return("WorkId:"+workId+" time:"+time + " Depends on:"+dependStr+" Child:"+childStr);
+        if (childStr.length() > 1)
+            childStr = childStr.substring(0, childStr.length() - 1);
+
+        childStr += "]";
+
+        return ("WorkId:" + workId + " time:" + time + " Depends on:" + dependStr + " Child:" + childStr);
     }
-
-
-
-
 }

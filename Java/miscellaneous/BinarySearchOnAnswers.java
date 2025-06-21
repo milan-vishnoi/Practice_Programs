35f1109c15b42afa07f540c5cbc7eff0a9185351
Milan Vishnoi <milan.vish10@gmail.com>
2025-06-22 03:15:34 +0530

    Shifted the problem from one file to other.

--- Java/miscellaneous/BinarySearchOnAnswers.java
+++ Java/miscellaneous/BinarySearchOnAnswers.java
@@ -259,5 +259,27 @@ public class BinarySearchOnAnswers {
         }
         return low;
     }
+
+    // Leetcode problem https://leetcode.com/problems/kth-missing-positive-number/
+    public static int findKthPositive(int[] arr, int k) {
+
+        int low = 0;
+        int high = arr.length-1;
+        int mid=0, missing=0;
+
+        while(low<=high)
+        {
+            mid = low +(high-low)/2;
+            missing = arr[mid] - (mid+1);
+            if(missing<k)
+            low = mid+1;
+            else
+            high = mid-1;
+
+        }
+
+        return low+k;
+        
+    }
     
 }

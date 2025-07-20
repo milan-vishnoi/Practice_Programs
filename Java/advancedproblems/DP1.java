85878da564081e41aed4920a6d379b7c2314e4ca
Milan Vishnoi <milan.vish10@gmail.com>
2025-07-21 02:09:15 +0530

    Added space optimized version of Cricular House Robber problem

--- Java/advancedproblems/DP1.java
+++ Java/advancedproblems/DP1.java
@@ -84,35 +84,36 @@ public class DP1 {
     public static int robCircular(int[] nums) {
 
         int n = nums.length;
-        
+
         if(n==1)
         return nums[0];
         else if(n==2)
         return Math.max(nums[0],nums[1]);
-        int memo[] = new int[n+1];
-
-        // First calculate for 0 to n-2
-        memo[0] = 0;
-        memo[1] = nums[0];
-        int result=0;
-
-        for(int i=1;i<n-1;i++)
-        memo[i+1] = Math.max(memo[i],memo[i-1]+nums[i]);
-
-        // Saving the max value of 0 to n-2 houses
-        result = memo[n-1];
         
-        // Resetting the value of array
-        Arrays.fill(memo,0);
+        int prev1=0;
+        int prev2=0;
+        int curr=0;
+        int max1=0;
 
-        // Calculating for 1 to n-1
-        memo[2] = nums[1];
+        for(int i=0;i<n-1;i++)
+        {
+           curr = Math.max(prev1,prev2+nums[i]);
+           prev2=prev1;
+           prev1=curr;
+        }
 
-        for(int i=2;i<n;i++)
-        memo[i+1] = Math.max(memo[i],memo[i-1]+nums[i]);
+        max1 = curr;
+        prev1=0;
+        prev2=0;
 
+        for(int i=1;i<n;i++)
+        {
+            curr = Math.max(prev1,prev2+nums[i]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
 
-        return Math.max(memo[n],result);
+        return Math.max(max1,curr);
     }
     
 }

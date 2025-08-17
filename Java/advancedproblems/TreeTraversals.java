751c935ad0307476ea7496c13e4132f7db6bd6d0
Milan Vishnoi <milan.vish10@gmail.com>
2025-08-18 02:01:22 +0530

    Added Level Order Tree Traveral (BFS)

--- Java/advancedproblems/TreeTraversals.java
+++ Java/advancedproblems/TreeTraversals.java
@@ -1,7 +1,9 @@
 package advancedproblems;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Stack;
 
 
@@ -36,8 +38,8 @@ public class TreeTraversals {
     }
 
     /* Using Stack and iteration
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        //List<Integer> result = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
         TreeNode current = root;
 
@@ -51,14 +53,12 @@ public class TreeTraversals {
             // process the node
             current = stack.pop();
             result.add(current.val);
-
             // now go right
             current = current.right;
         }
 
         return result;
-    }
-     */
+    }*/
 
 
     //Leetcode problem https://leetcode.com/problems/binary-tree-preorder-traversal/
@@ -74,6 +74,27 @@ public class TreeTraversals {
         return result;
         
     }
+ 
+    /*
+    List<Integer> preorderIter(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+    if (root == null) return result;
+
+    Stack<TreeNode> stack = new Stack<>();
+    stack.push(root);
+
+    while (!stack.isEmpty()) {
+        TreeNode node = stack.pop();
+        result.add(node.val);  // visit root (same as recursion)
+
+        // Push right first (so left is processed first)
+        if (node.right != null) stack.push(node.right);
+        if (node.left != null) stack.push(node.left);
+    }
+    return result;
+} */
+
+
 
     //Leetcode problem https://leetcode.com/problems/binary-tree-postorder-traversal/
     public static List<Integer> postorderTraversal(TreeNode root) {
@@ -86,6 +107,72 @@ public class TreeTraversals {
 
         return result;
         
+    }
+    /* 
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+        stack1.push(root);
+
+        while (!stack1.isEmpty()) {
+            TreeNode node = stack1.pop();
+            stack2.push(node);
+
+            if (node.left != null) stack1.push(node.left);
+            if (node.right != null) stack1.push(node.right);
+        }
+
+        while (!stack2.isEmpty()) {
+            res.add(stack2.pop().val);
+        }
+
+        return res;
+    }*/
+
+
+    //Leetcode Problem https://leetcode.com/problems/binary-tree-level-order-traversal/
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root==null)
+        return result;
+        TreeNode[] nodeQueue = new TreeNode[2000];
+        int front=-1,rear=-1;
+        //Queue<TreeNode> nodeQueue = new ArrayDeque<>();
+        List<TreeNode> nodeList;
+        List<Integer> valueList;
+        TreeNode node;
+
+        //nodeQueue.add(root);
+        nodeQueue[++rear] = root;
+        front++;
+        while(front<=rear)
+        {
+           nodeList = new ArrayList<>();
+           valueList= new ArrayList<>();
+        //   while(!nodeQueue.isEmpty())
+        //    nodeList.add(nodeQueue.poll());
+         while(front<=rear)
+          nodeList.add(nodeQueue[front++]);
+
+          for(int i=0;i<nodeList.size();i++)
+          {
+           node = nodeList.get(i);
+           valueList.add(node.val);
+           if(node.left!=null)
+           nodeQueue[++rear] = node.left;
+           //nodeQueue.add(node.left);
+           if(node.right!=null)
+           nodeQueue[++rear] = node.right;
+           //nodeQueue.add(node.right);
+          }
+
+           result.add(new ArrayList<>(valueList));
+        }
+
+        return result;
     }
     
 }
